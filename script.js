59b6078d8c3ef72ad9aef166eb3ccd0dc2ddc88e
M Abdullah <150430204+connect-abdullah@users.noreply.github.com>
2024-12-13 03:08:55 +0500

    Planetary Weight Calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+const calBtn = document.getElementById('calculate')
+calBtn.addEventListener('click', function() {
+    const weight = parseFloat(document.getElementById('weight').value)
+    const planet = parseFloat(document.getElementById('planet').value)
+    const result = document.getElementById('result')
+
+    if(isNaN(weight)) {
+        result.innerHTML = "Please Enter Correct Weight !!!";
+        return result
+    }
+
+    if(weight === 0) {
+        result.innerHTML = "0 !!! Eat Something ... ";
+        return result
+    }
+
+   const planetaryWeight =  weight * planet.toFixed(2)
+   return result.innerHTML = `Your Weight Selected Planet is ${planetaryWeight}`
+})
